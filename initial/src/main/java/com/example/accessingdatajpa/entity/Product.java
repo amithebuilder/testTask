10efef867b7ade6f2df04e3188a8621db2e4f79e
Troyanov Alexandr <coconutforworking@gmail.com>
2025-04-02 21:53:20 +0300

    task-1 : Добавлена сущность Product, а также вспомогательные AuditingEntity и BaseEntity.

---
+++ initial/src/main/java/com/example/accessingdatajpa/entity/Product.java
@@ -0,0 +1,31 @@
+package com.example.accessingdatajpa.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Table(name = "products")
+public class Product extends AuditingEntity {
+
+    @Column(name = "name", nullable = false, length = 100)
+    private String name;
+
+    @Column(name = "description", columnDefinition = "TEXT")
+    private String description;
+
+    @Column(name = "price", precision = 10, scale = 2)
+    private BigDecimal price;
+}
